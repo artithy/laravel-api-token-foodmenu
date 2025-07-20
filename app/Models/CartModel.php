ab54495d23b824513267a69bde3bae8a362cf0ea
Afrina Tithy <afrina.tithy@gmail.com>
2025-07-20 23:41:00 +0600

    updated cart

---
+++ app/Models/CartModel.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class CartModel extends Model
+{
+    protected $table = 'carts';
+
+    protected $fillable = [
+        'cart_token',
+        'status',
+    ];
+
+      public function items()
+    {
+        return $this->hasMany(\App\Models\CartItemModel::class, 'cart_id', 'id');
+    }
+}
